0aa2be12e35be2b5ce06a4586713b85767c77c9b
Alejandro Aguilar Higuera <alejandro.aguilar@onephase.com>
2023-11-18 14:57:07 -0600

    init

---
+++ src/navigation/NavigationStack.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '~/screens/HomeScreen';
+import LoginScreen from '~/screens/LoginScreen';
+
+
+const Stack = createStackNavigator();
+
+export default function NavigationStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    )
+
+}
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import NavigationStack from './src/navigation/NavigationStack';
 
-export default function App() {
+export default function LoginScreen() {
   return (
-    <NavigationContainer>
-    <NavigationStack />
-    </NavigationContainer>
-  
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
